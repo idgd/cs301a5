9eefcba3ec0a64a00b58855b46783e65c9c65343
idgd <isaac.dudney@icloud.com>
2018-03-20 12:50:26 -0600

    added insertion sort

---
+++ insertion.py
@@ -0,0 +1,18 @@
+def insertion(l):
+	# return list
+	r = []
+	# while l exists
+	while l:
+		# value: pop the end
+		v = l.pop()
+		# index: 0
+		i = 0
+		# for f in return list
+		for f in r:
+			# until it finds where it's supposed to go, i++
+			if f <= v:
+				i += 1
+		# insert it at the proper location
+		r.insert(i,v)
+	# return list
+	return(r)
