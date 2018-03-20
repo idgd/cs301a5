2487df63b832fe1ef4c34a4337a4b3585ee1ab19
idgd <isaac.dudney@icloud.com>
2018-03-20 12:57:48 -0600

    added bubble sort

---
+++ bubble.py
@@ -0,0 +1,8 @@
+def bubble(l):
+	# run through list n * n times (f,g)
+	for f in range(len(l)):
+		for g in range(len(l) - 1):
+			# if the current one is greater than the next one
+			if l[g] > l[g + 1]:
+				# swap them
+				l[g], l[g + 1] = l[g + 1], l[g]
