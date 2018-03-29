2f70d74f29b56e8cb87783f2c01e0e400811a537
idgd <isaac.dudney@icloud.com>
2018-03-29 11:36:38 -0600

    fixed selection to be in-place; added timing to test

--- test.py
+++ test.py
@@ -4,17 +4,28 @@ import bubble
 import selection
 import merge
 
+from time import time
 from itertools import permutations
 
 class TestSort(unittest.TestCase):
 	def bench(self,f):
 		l = [f for f in range(-4,5)]
-		for g in range(10):
+		a = 0
+		c = 0
+		for g in range(9):
 			for h in permutations(l,g):
 				h = list(h)
 				s = h[:]
 				s.sort()
+
+				start = time()
 				self.assertEqual(f(h),s)
+				end = time()
+				a += (end - start)
+				c += 1
+
+		a /= c
+		print("\n" + str(f.__name__) + ": {:.8f}".format(a))
 
 	def test_insert(self):
 		self.bench(insertion.insertion)
