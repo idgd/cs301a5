6f3a5b25ede8013c22c3805b0539336838ab4e01
idgd <isaac.dudney@icloud.com>
2018-04-03 10:23:02 -0600

    fixed time code as per class

--- time.py
+++ time.py
@@ -12,24 +12,18 @@ def bench(f,l):
 	a = 0
 	c = 8
 	for g in range(c):
-		shuffle(l)
-
-		# many repeated runs of sort algos are exponentially faster
-		# most likely because of runtime optimizations
-		# with this, time averages are much closer to individual run times
-		flush = [f for f in range(111111)]
-		shuffle(flush)
-		flush.sort()
+		nl = l[:]
+		shuffle(nl)
 
 		s = time()
-		f(l)
+		f(nl)
 		e = time()
 
 		a += (e - s)
 
 	return(a / c)
 
-l = [f for f in range(16384)]
+l = [f for f in range(2048)]
 
 t = bench(insertion.insertion,l)
 print("Insertion time: {0:.8f}".format(t))
@@ -44,10 +38,16 @@ t = bench(merge.merge,l)
 print("Merge time:     {0:.8f}".format(t))
 
 # benchmark results:
-# Insertion time: 0.69230759
-# Bubble time:    0.00000831
-# Selection time: 0.00000817
-# Merge time:     0.00000638
-# as expected, merge is the fastest, but surprisingly,
-# only by a slim margin over selection sort
-# insertion is also many orders of magnitude slower
+# Insertion time: 0.09163362
+# Bubble time:    0.55817613
+# Selection time: 0.12464038
+# Merge time:     0.00558105
+# as expected, merge is the fastest
+# bubble is slowest, selection second slowest, and insertion third
+# bubble doesn't use builtin functions, it uses assignment
+# probably why it's slowest vs selection
+# selection creates a new list and inserts into it, so probably more ops than
+# insertion
+# insertion operates in place with insert, so uses builtins, so very fast
+# merge has better big-O runtime, overwhelming any small optimizations over
+# the long run
